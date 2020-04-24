7ff2fe64dda4272403592f93c26e192582ae63ed
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-24 02:40:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  counter = 0
+  yer_mom = []
+  while counter < array.length
+    yer_mom << yield (array[counter])
+    counter += 1
+  end
+  yer_mom
+end
 
-
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(names) do |name|
+  name.split(" ").first
+end
